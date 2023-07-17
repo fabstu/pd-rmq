c489d68b3c4a1a3cce0c92f02e05d32a053baf9f
fabstu <fabian.sturm@aduu.de>
2023-07-17 11:36:49 +0200

    Refactoring.

--- src/rmq/mod.rs
+++ src/rmq/mod.rs
@@ -1,3 +1,9 @@
+mod naive_fast;
+mod naive_slow;
+
+use naive_fast::RMQNaiveFast;
+use naive_slow::RMQNaiveSlow;
+
 use std::error::Error;
 use std::fmt;
 
@@ -31,49 +37,20 @@ impl fmt::Display for RMQError {
 
 impl Error for RMQError {}
 
-#[derive(MallocSizeOf, Clone)]
-pub struct RMQNaive {
-    naive: Vec<Vec<u64>>,
-}
-
-impl RMQNaive {
-    pub fn new(numbers: &mut Vec<u64>) -> Self {
-        let n = numbers.len();
-
-        // O(u^2). Could do u log n using consecutively less space for "to".
-        //
-        let mut naive = vec![vec![std::u64::MAX; n as usize]; n as usize];
-
-        for i in 0..n {
-            // Minimum to itself.
-            naive[i][i] = numbers[i];
-
-            for j in i + 1..n {
-                // We only grow, so with each step we take on a new number.
-                //
-                // If the new number is smaller than the current, then record
-                // it. Else keep the current minimum.
-                naive[i as usize][j as usize] = std::cmp::min(naive[i][j - 1], numbers[j]);
-            }
-        }
-
-        return Self { naive: naive };
-    }
-
-    pub fn range_minimum_query(&self, from: usize, to: usize) -> Result<u64, RMQError> {
-        // Stay in range.
-        assert!(from < self.naive.len());
-        assert!(to < self.naive.len());
-
-        return Ok(self.naive[from][to]);
-    }
-}
-
 pub fn rmq(path: &Path) {
     println!("rmq");
+
+    let path = Path::new("testdata/rmq_examples/rmq_example_1.txt");
+
+    benchmark_and_check::<RMQNaiveFast>(path, None);
 }
 
-pub fn benchmark_and_check(path: &Path, want: Option<Vec<u64>>) {
+pub trait RMQ {
+    fn new(numbers: &Vec<u64>) -> Self;
+    fn range_minimum_query(&self, from: usize, to: usize) -> Result<u64, RMQError>;
+}
+
+pub fn benchmark_and_check<T: RMQ + MallocSizeOf>(path: &Path, want: Option<Vec<u64>>) {
     println!("rmq");
 
     let instance = instances::read_rmq_instance(path).unwrap();
@@ -81,7 +58,7 @@ pub fn benchmark_and_check(path: &Path, want: Option<Vec<u64>>) {
     // Check correctness.
     if let Some(want) = want {
         let mut numbers = instance.numbers.clone();
-        let rmq = RMQNaive::new(&mut numbers);
+        let rmq = T::new(&mut numbers);
 
         for (i, query) in instance.queries.clone().iter().enumerate() {
             println!("Query nr {}: {:?}", i, query);
@@ -95,16 +72,16 @@ pub fn benchmark_and_check(path: &Path, want: Option<Vec<u64>>) {
     }
 
     // Start benchmark
-    benchmark(instance);
+    benchmark::<T>(instance);
 }
 
-fn benchmark(instance: RMQInstance) {
+fn benchmark<T: RMQ + MallocSizeOf>(instance: RMQInstance) {
     // Clone numbers because we sort them.
     let mut numbers = instance.numbers.clone();
 
     let start = Instant::now();
 
-    let rmq = RMQNaive::new(&mut numbers);
+    let rmq = T::new(&mut numbers);
 
     let queries_count = instance.queries.len();
 
@@ -137,5 +114,5 @@ fn benchmark(instance: RMQInstance) {
 fn testing_rmq_benchmark1() {
     let path = Path::new("testdata/rmq_examples/rmq_example_1.txt");
 
-    benchmark_and_check(path, None);
+    benchmark_and_check::<RMQNaiveFast>(path, None);
 }
